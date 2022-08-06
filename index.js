75974c338be2069c7c222ef6b8d8fd4e533a115d
Neuryhdez45 <84157785+Neuryhdez45@users.noreply.github.com>
2022-08-06 13:22:40 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+   <head>
+
+   </head>
+   <body>
+    <h1>COMO CREAR UN BOT DE DISCORD?</h1>
+    <a href="https://discord.gg/zCffM5QUUN">Mi discord</a>
+    <p>Para crear un bot de discord, hay que tener en cuenta esto > </p>
+   </body>
+</html>
